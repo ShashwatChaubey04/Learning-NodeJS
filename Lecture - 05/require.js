829985a1a8c3f376780406f659bdf11ac522f5d6
Shashwat Chaubey <chaubeyshashwat04062005@gmail.com>
2025-06-26 11:49:14 +0530

    Lecture - 05

--- Lecture - 05/require.js
+++ Lecture - 05/require.js
@@ -2,20 +2,21 @@
 
 When we write { require("./fileName") => Requiring a File } multiple things happen's :
 
-1) Resolving the Modules.
+1) Resolving the Modules
    => ./localpath
    => .json
    => node : module
 
-2) Loading the Module.
+2) Loading the Module
    => File content is loaded according to File types. (get the Data of the File)
 
-3) Wraps the Data inside IIFE.
+3) Compiling the Module
+   => Wraps the Data inside IIFE.
 
-4) Evaluation.
+4) Evaluating the Module
    => Code is executed and module.exports is Returned.
 
-5) Caching
+5) Caching the Module
    => If we require a specific Module in more than 1 Files and then run a File other 4 steps will executes and the Module cached up and If we run another Code containing the same Module it will not run the other four step. (as that module is cached up by Node JS)
 
 
